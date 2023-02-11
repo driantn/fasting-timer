f819bb384062d2187c5bc13221a6a427a807f720
Driant Naun <driantn@gmail.com>
2023-02-11 22:34:14 +0100

    added progress

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -1,4 +1,6 @@
 import useLocalStorage from 'use-local-storage';
+import classNames from 'classnames';
+import { twMerge } from 'tailwind-merge';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import utc from 'dayjs/plugin/utc';
@@ -10,16 +12,33 @@ dayjs.extend(relativeTime);
 
 dayjs.tz.setDefault('Europe/Berlin');
 
+const low = 5;
+const med = 10;
+const high = 15;
+
 export const TimerBubble = () => {
-  const now = () => dayjs().tz().format();
-  const [timer, setTimer] = useLocalStorage('fasting-time', now());
+  const now = () => dayjs().tz();
+  const [timer, setTimer] = useLocalStorage('fasting-time', now().format());
+  const parsedTimer = dayjs(timer).tz();
+  const diff = now().diff(parsedTimer, 'hours');
 
   return (
-    <button
-      onClick={() => setTimer(now())}
-      className="rounded-full w-[200px] h-[200px] flex flex-col justify-center items-center bg-green-500 font-bold"
+    <div
+      className={twMerge(
+        classNames(
+          'rounded-full w-[200px] h-[200px] flex flex-col justify-center items-center font-bold text-white border-white border border-spacing-1',
+          {
+            'bg-orange-500': diff <= low,
+            'bg-yellow-500': diff > low && diff <= med,
+            'bg-blue-500': diff > med && diff <= high,
+            'bg-green-500': diff >= high,
+          },
+        ),
+      )}
     >
-      {dayjs(timer).from(dayjs(now()))}
-    </button>
+      <button onClick={() => setTimer(now().format())}>
+        {dayjs(timer).from(dayjs(now()))}
+      </button>
+    </div>
   );
 };
