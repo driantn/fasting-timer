405ab734405162dde2975f167b76c03e79d9618e
Driant Naun <driantn@gmail.com>
2023-02-11 16:35:34 +0100

    added base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,41 +3,44 @@ import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Fasting timer',
-        description: 'Simple fasting timer',
-        theme_color: '#075985',
-        background_color: '#075985',
-        icons: [
-          {
-            src: '128x128.png',
-            sizes: '128x128',
-            type: 'image/png',
-          },
-          {
-            src: '256x256.png',
-            sizes: '256x256',
-            type: 'image/png',
-          },
-          {
-            src: '512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-      devOptions: {
-        enabled: true,
-      },
-    }),
-  ],
-  server: {
-    host: 'localhost',
-    port: 3000,
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'local' ? '/' : '/fasting-timer',
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'Fasting timer',
+          description: 'Simple fasting timer',
+          theme_color: '#075985',
+          background_color: '#075985',
+          icons: [
+            {
+              src: '128x128.png',
+              sizes: '128x128',
+              type: 'image/png',
+            },
+            {
+              src: '256x256.png',
+              sizes: '256x256',
+              type: 'image/png',
+            },
+            {
+              src: '512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+          ],
+        },
+        devOptions: {
+          enabled: true,
+        },
+      }),
+    ],
+    server: {
+      host: 'localhost',
+      port: 3000,
+    },
+  };
 });
